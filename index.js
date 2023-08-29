797c9b0110d592af17b26b5939d8beb248a5902f
RikelmVoltz <142436269+RikelmVoltz@users.noreply.github.com>
2023-08-29 16:37:57 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+// rikelme voltz
+
+// A. Exercícios de interpretação de código
+
+
+// resposta da 1:   
+
+// será impresso na tela o resultado f 9
+
+
+// resposta da 2: será empresso no console a frase da pessoa em maiscúlo,ira trocar o "A" por "I" e ira contar quantos caracteres vai ter na sua frase.
+
+// B . Exercícios de escrita de código
+
+1 
+/*
+let email = prompt("digite seu email");
+let nome = prompt("digite seu nome"); 
+console.log(" O e-mail " + email + " foi registrado com sucesso " +  " seja bem vindo(a) "  + nome );
+
+*/
+
+2
+/*
+
+
+const comida = ["pizza" , "cachorro quente" , "arroz de forno" , "frango" , "xis"];
+console.log(comida);
+console.log(" essas são minhas comidas preferidas " );
+console.log(comida [0]);
+console.log(comida [1]);
+console.log(comida [2]);
+console.log(comida [3]);
+console.log(comida [4]);
+*/
+
+3
+/*
+let listaDeTarefas =[];
+let tarefas = prompt("você realiza tarefas no dia dia?cite 3 delas");
+listaDeTarefas.push("lavar a louça");
+listaDeTarefas.push("arrumar a cama");
+listaDeTarefas.push("aspirar a casa");  
+console.log(listaDeTarefas);
+let digite = Number(prompt("você realizou uma hoje?qual delas [0 a 2]"));
+console.log(listaDeTarefas.splice(digite,1));
+console.log(listaDeTarefas);
+*/             
